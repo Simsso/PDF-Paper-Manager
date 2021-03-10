8b5fdc1284901de72bc10eede1740d9d1e603c3e
Timo Denk <github@timodenk.com>
2021-03-10 22:57:28 +0100

    refactor: extract parser into separate file

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const fs = require('fs');
 
-const types = require('./types');
-const Paper = types.Paper;
+const parser = require('./parser');
 
 
 const paperPath = process.env.PAPER_DIR;
@@ -18,50 +17,12 @@ async function getPaperList() {
     });
 }
 
-function parsePaperFileName(fileName) {
-    const fileExtension = '.pdf'
-    if (!fileName.endsWith(fileExtension)) {
-        throw new Error(`Paper title must end with '${fileExtension}', got ${fileName}`);
-    }
-
-    // remove file extension
-    const fileNameWithoutExtension = fileName.slice(0, -fileExtension.length);
-
-    // parse year
-    const yearRegExp = /\s\(\d{4}\)\s/;
-    let yearString = fileNameWithoutExtension.match(yearRegExp)[0].trim();
-    const year = parseInt(yearString.slice(1, -1), 10);
-
-    const [authorsString, titleAndTagsString] = fileNameWithoutExtension.split(yearRegExp);
-
-    // parse tags
-    const tagsRegExp = /\[.*\]/
-    const tagsMatch = titleAndTagsString.match(tagsRegExp);
-    let tags = [];
-    if (tagsMatch) {
-        tags = tagsMatch[0].slice(1, -1).split(',');
-        tags = tags.map(s => s.trim());
-    }
-
-    // parse title
-    const title = titleAndTagsString.replace(tagsRegExp, '').trim();
-
-    // parse authors
-    const etAlRegExp = /\set al\.$/;
-    const hasEtAl = authorsString.endsWith(' et al.');
-    const authorsStringWithoutEtAl = authorsString.replace(etAlRegExp, '');
-    const andAuthorSeparatorRegExp = /\sand\s/
-    const authors = authorsStringWithoutEtAl.split(andAuthorSeparatorRegExp);
-
-    return new Paper(fileName, authors, hasEtAl, year, title, tags);
-}
-
 
 (async () => {
     try {
         const paperList = await getPaperList();
         console.log(paperList);
-        console.log(paperList.map(parsePaperFileName))
+        console.log(paperList.map(parser.paperFileName))
     } catch (e) {
         console.error("Failed");
         console.error(e);
---
+++ src/parser.js
@@ -0,0 +1,45 @@
+const types = require('./types');
+const Paper = types.Paper;
+
+
+function paperFileName(fileName) {
+    const fileExtension = '.pdf'
+    if (!fileName.endsWith(fileExtension)) {
+        throw new Error(`Paper title must end with '${fileExtension}', got ${fileName}`);
+    }
+
+    // remove file extension
+    const fileNameWithoutExtension = fileName.slice(0, -fileExtension.length);
+
+    // parse year
+    const yearRegExp = /\s\(\d{4}\)\s/;
+    let yearString = fileNameWithoutExtension.match(yearRegExp)[0].trim();
+    const year = parseInt(yearString.slice(1, -1), 10);
+
+    const [authorsString, titleAndTagsString] = fileNameWithoutExtension.split(yearRegExp);
+
+    // parse tags
+    const tagsRegExp = /\[.*\]/
+    const tagsMatch = titleAndTagsString.match(tagsRegExp);
+    let tags = [];
+    if (tagsMatch) {
+        tags = tagsMatch[0].slice(1, -1).split(',');
+        tags = tags.map(s => s.trim());
+    }
+
+    // parse title
+    const title = titleAndTagsString.replace(tagsRegExp, '').trim();
+
+    // parse authors
+    const etAlRegExp = /\set al\.$/;
+    const hasEtAl = authorsString.endsWith(' et al.');
+    const authorsStringWithoutEtAl = authorsString.replace(etAlRegExp, '');
+    const andAuthorSeparatorRegExp = /\sand\s/
+    const authors = authorsStringWithoutEtAl.split(andAuthorSeparatorRegExp);
+
+    return new Paper(fileName, authors, hasEtAl, year, title, tags);
+}
+
+module.exports = {
+    paperFileName
+}
